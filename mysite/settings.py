c64eabab5b5f0fb435837d12e5b79c7e77a4429e
david <davidgomezaragon@hotmail.com>
2021-03-09 13:17:11 +0100

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+PROJECT_ROOT=os.path.abspath(os.path.dirname(__file__))
+STATICFILES_DIRS=(
+    os.path.join(PROJECT_ROOT,'static'),
+)
